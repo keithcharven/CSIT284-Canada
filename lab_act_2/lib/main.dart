b50458c069ebe08d746b76b7def4a96a3bc3fbd2
LAB12_USER ON WS09 <L12X19W09@LabHCI.net>
2026-08-19 20:00:32 +0800

    Add gradient background

--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+              Colors.blue,
+              Colors.red
+            ])
+          ),
+          child: Center(
+            child: Text(
+              'Hello World'),
+          ),
+        ),
+      ),
+   ),
+  );
 }
